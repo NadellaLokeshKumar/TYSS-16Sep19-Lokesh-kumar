dd9d0649e4464966837d9c692a375d371b7003f8
NadellaLokeshKumar <lokeshnadella99@gmail.com>
2019-11-28 09:58:02 +0530

    file handling

---
+++ filehandling/src/com/ty/filehandling/FileReadingClass.java
@@ -0,0 +1,25 @@
+package com.ty.filehandling;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadingClass {
+
+	public static void main(String[] args) {
+		
+		String path =  "C:\\Users\\Naveen Kumar\\Desktop\\text.txt.txt";
+		FileReader reader = null;
+		
+		try {
+			 reader = new FileReader(path);
+			int i;
+			 while((i=reader.read())!=-1) {
+			 
+				 System.out.print((char)i);
+			 }
+		} catch (IOException e) {
+		
+			e.printStackTrace();
+		}
+	}
+}
---
+++ filehandling/src/com/ty/filehandling/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Naveen Kumar
+ *
+ */
+package com.ty.filehandling;
