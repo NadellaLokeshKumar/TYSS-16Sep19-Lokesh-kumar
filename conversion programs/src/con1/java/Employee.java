53f02893c9455a21ddf0df29f2f5addc1ec92254
NadellaLokeshKumar <lokeshnadella99@gmail.com>
2019-11-28 10:08:52 +0530

    conversion programs

---
+++ conversion programs/src/con1/java/Employee.java
@@ -0,0 +1,22 @@
+package con1.java;
+import java.util.Scanner;
+
+public class Employee {
+
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+		int a=10;
+		int b=11;
+		int c=a+b;
+		System.out.println(c);
+	}
+	public static void main(int[] args) {
+		int x=10;
+		System.out.println(x);
+	}
+	public static void main(double[] args) {
+		int y=34;
+		System.out.println(y);
+	}
+
+}
---
+++ conversion programs/src/con1/java/Student.java
@@ -0,0 +1,12 @@
+package con1.java;
+
+public class Student {
+
+	public static void main(Double[] args) {
+     double a=10.00;
+     double b=11.00;
+     double c=a+b;
+		System.out.println("The sum of a+b is "+c);
+	}
+
+}
